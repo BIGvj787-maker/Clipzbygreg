e03aa1f43c431762c4513b2c4b7a3227babc36a1
BIGvj787-maker <BIGvj787@icloud.com>
2026-09-06 22:47:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "Clipz by Greg is running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
